287201418a868ecd9f1e67ffb1ddfe114a662e67
Pedro H. C <pedrohcm5@gmail.com>
2020-12-21 22:27:11 -0300

    Add files via upload

---
+++ remove.js
@@ -0,0 +1,113 @@
+let salvos = document.getElementsByClassName("_9AhH0");
+console.log(salvos);
+
+for (let i = 0; i < salvos.length; i++){
+
+    let remover = document.getElementsByClassName("wpO6b ");
+    let confirmaRemover = document.getElementsByClassName("aOOlW  bIiDR");
+    salvos[i].click();
+    remover[4].click();
+    remover[30].click();
+
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+while (j < 3) {
+
+    salvos[j].click();
+    sleep(2000);
+    remover[5].click();
+    sleep(2000);
+    remover[remover.length - 1].click();
+    sleep(2000);
+
+}
+
+let j = 0;
+let timer = 0;
+let salvos = document.getElementsByClassName("_9AhH0");
+let remover = document.getElementsByClassName("wpO6b ");
+
+function Remover() {
+    setTimeout(() => {  salvos[j].click(); }, 200);
+    setTimeout(() => {  remover[5].click(); }, 800);
+    setTimeout(() => {  remover[remover.length - 1].click(); }, 900);
+    setTimeout(() => {  j++; }, 800);
+}
+ 
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
+setTimeout(() => {  Remover(); }, timer);
+timer += 1500;
