2b77bac90b48e86fdf782654e01adb835eb8102b
Pedro H. C <pedrohcm5@gmail.com>
2020-12-22 09:08:31 -0300

    Update remove.js

--- remove.js
+++ remove.js
@@ -4,13 +4,20 @@ let timer = 0;  // Tempo
 let salvos = document.getElementsByClassName("_9AhH0");     // Encontrar os salvos
 let remover = document.getElementsByClassName("wpO6b ");    // Encontrar botoes de remover dos favs e fechar
 
-function Remover() {
-    setTimeout(() => {  salvos[j].click(); }, 200);
-    setTimeout(() => {  remover[5].click(); }, 800);
-    setTimeout(() => {  remover[remover.length - 1].click(); }, 900);
-    setTimeout(() => {  j++; }, 800);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Copiar e colar quantos favoritos tiver
-setTimeout(() => {  Remover(); }, timer);
-timer += 1500;
+async function removeScript() {
+    while (j < salvos.length) {
+      salvos[j].click();
+      await sleep(700);
+      remover[5].click();
+      await sleep(200);
+      remover[remover.length - 1].click();
+      await sleep(200);
+      j++;
+  }
+}
+
+removeScript();
